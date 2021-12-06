96ae430c9526222a47dc7aff05b8d1393d367870
FlorianMrn <fmerrien.dev@gmail.com>
2021-12-06 11:51:47 +0100

    Checkout
    Beginning Checkout component

--- src/components/Card/index.tsx
+++ src/components/Card/index.tsx
@@ -1,27 +1,27 @@
-import './card.css';
+import "./card.css";
 
 type CardProps = {
-    title: string,
-    description: string,
-    price: number,
-    img: string,
-    setter: any,
-    id: number
-}
+  title: string;
+  description: string;
+  price: number;
+  img: string;
+  setter: any;
+  id: number;
+};
 
-const Card = ({title, description, price, img, setter, id }: CardProps) => {
-
-
-    return (
-        <div className="card" key={id}>
-            <img src={img} alt="Avatar" id="cardImg"/>
-            <div className="container">
-                <h4><b>{title}</b></h4>
-                <p>{description}</p>
-                <button onClick={() => setter(id)}>Ajouter</button>
-            </div>
-        </div>
-    )
+const Card = ({ title, description, price, img, setter, id }: CardProps) => {
+  return (
+    <div className="card" key={id}>
+      <img src={img} alt="Avatar" id="cardImg" />
+      <div className="container">
+        <h3>
+          <b>{title}</b>
+        </h3>
+        <p>{description}</p>
+        <button onClick={() => setter(id)}>Ajouter</button>
+      </div>
+    </div>
+  );
 };
 
 export default Card;
